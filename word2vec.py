03d2b8110057183593ef5c964f068845b87506f8
s0ralit <s0ralit@protonmail.com>
2019-01-16 23:34:37 +0900

    Update

--- word2vec.py
+++ word2vec.py
@@ -6,14 +6,12 @@ import json
 import glob
 import re
 import sys
-from prettyprint import pp
-reload(sys)
-sys.setdefaultencoding('utf-8')
-
+import time
 
 
 def int_to_onehot(indices, depth):
     return tf.reshape(tf.transpose(tf.one_hot(indices, depth)), [depth, 1])
+
 
 def numerical_sort(value):
     numbers = re.compile(r'(\d+)')
@@ -29,8 +27,13 @@ def read_json_to_list(folder_path):
         with open(file, 'r') as f:
             f = f.read()
             dict = json.loads(f, "utf-8")
-            for i in dict:
-                text_list.append(i)
+            for key, value in sorted(dict.items(), key=lambda x: x[1]):
+                if key == "":
+                    pass
+                else:
+                    text_list.append(key)
+                    if value > 10:
+                        break
     return text_list
 
 
@@ -45,27 +48,36 @@ def read_json_to_dict(folder_path):
 
 
 def dict_value_to_onehot(dict):
-    last_value =  sorted(dict.values())[-1]
+    dicts = {}
+    last_value = sorted(dict.values())[-1]
     for key, value in sorted(dict.items(), key=lambda x: x[1]):
-        dict[key] = int_to_onehot(value-1, last_value)
-        print key, value
+        if value == 1:
+            pass
+        else:
+            dicts[key] = int_to_onehot(value-2, 10)
+            if value > 100:
+                break
+    return dicts
 
-texts = read_json_to_list("./outputs/*")
-print "coverted list!"
-dict = read_json_to_dict("./outputs/*")
-print "converted dict!"
+
+texts = read_json_to_list("./test/*")
+print("coverted list!")
+dict = read_json_to_dict("./test/*")
+print("converted dict!")
 text_ids = dict_value_to_onehot(dict)
-print "converted onehot!"
+# text_ids = json.dumps(str(text_ids), ensure_ascii=False, indent=2)
+print("converted onehot!")
+
 
 read_json_to_dict("./outputs/*")
 
-w = tf.Variable(tf.random_normal([10, 10], -0.01, 0.01))
+w = tf.Variable(tf.random_normal([10, 10], -0.1, 0.1))
 
 x1 = tf.placeholder(tf.float32, [10, 1])
 
 x2 = tf.placeholder(tf.float32, [10, 1])
 
-b = tf.Variable(tf.random_normal([10, 1], -0.01, 0.01))
+b = tf.Variable(tf.random_normal([10, 1], -0.1, 0.1))
 
 y = tf.placeholder(tf.float32, [10, 1])
 
@@ -75,17 +87,15 @@ init = tf.global_variables_initializer()
 
 loss = tf.reduce_mean(tf.square(y - y_))
 
-train = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
+train = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
 
+# for i in range(1000000):
+#     for i in range(len(texts) - 2):
+#         print i
+#
 with tf.Session() as sess:
     sess.run(init)
-    print(sess.run(loss , feed_dict={
-        x1: sess.run(text_ids[texts[1]]),
-        x2: sess.run(text_ids[texts[3]]),
-        y: sess.run(text_ids[texts[2]])
-    }))
-    print("-----")
-    for i in range(1000000):
+    for i in range(1):
         for i in range(len(texts) - 2):
             sess.run(train, feed_dict={
                 x1: sess.run(text_ids[texts[i]]),
@@ -97,5 +107,4 @@ with tf.Session() as sess:
                 x2: sess.run(text_ids[texts[i+2]]),
                 y: sess.run(text_ids[texts[i+1]])
             }))
-
     print(sess.run(w))
